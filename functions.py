37b1a25663a3ce84d8f9ca604070e68d75b5e68e
Mohammad Niaei Shameoni <m.shemuni@myrafproject.org>
2013-02-24 22:45:18 +0000

    Photometry startup added. Bias, Dark, Flat checks activated. Avoid the user from mistakes on photometry tab.

--- functions.py
+++ functions.py
@@ -2,17 +2,20 @@ from PyQt4 import QtGui
 from PyQt4 import QtCore
 
 def add(self, flist, display):
-	filename=QtGui.QFileDialog.getOpenFileNames(self ,"Images...","",("Fit or Fits (*.fits *.fit)"))
-	a=flist.count()-1
+	filename = QtGui.QFileDialog.getOpenFileNames(self ,"Images...","",("Fit or Fits (*.fits *.fit)"))
+	a = flist.count()-1
 	for x in filename:
 		a=a+1
 		item = QtGui.QListWidgetItem()
 		flist.addItem(item)
 		item = flist.item(a)
 		item.setText(QtGui.QApplication.translate("Form", x, None, QtGui.QApplication.UnicodeUTF8))
+	fnumber = flist.count()
 	display.setText(QtGui.QApplication.translate("Form", str(fnumber) + " Files selected", None, QtGui.QApplication.UnicodeUTF8))
 
 def Rm(self, flist, display):
 	for x in flist.selectedItems():
 		flist.takeItem(flist.row(x))
+	fnumber = flist.count()
 	display.setText(QtGui.QApplication.translate("Form", str(fnumber) + " Files selected", None, QtGui.QApplication.UnicodeUTF8))
+	
--- main.py
+++ main.py
@@ -98,53 +98,116 @@ class MyForm(QtGui.QWidget):
     self.ui.pushButton_36.clicked.connect(self.PhotRm)
     self.ui.pushButton_60.clicked.connect(self.HeaderRm)
     
+    self.ui.checkBox.clicked.connect(self.UnlockBias)
+    self.ui.checkBox_2.clicked.connect(self.UnlockDark)
+    self.ui.checkBox_3.clicked.connect(self.UnlockFlat)
+    self.ui.checkBox_5.clicked.connect(self.HeaderGetVal)
     
+    self.ui.pushButton.clicked.connect(self.PhotometryGo)
+  
+  def PhotometryGo(self):
+	
+	if self.ui.listWidget.count() == 0:
+		QtGui.QMessageBox.critical( self,  ("Error"), ("Select IMAGE..."))
+	else:
+	
+		biaSta = self.ui.checkBox.checkState()
+		biaCou = self.ui.listWidget_2.count()
+		
+		darSta = self.ui.checkBox_2.checkState()
+		darCou = self.ui.listWidget_3.count()
+		
+		flaSta = self.ui.checkBox_3.checkState()
+		flaCou = self.ui.listWidget_4.count()
+		
+		if biaSta != QtCore.Qt.Checked and darSta != QtCore.Qt.Checked and flaSta != QtCore.Qt.Checked:
+			QtGui.QMessageBox.critical( self,  ("Error"), ("Nothing to do..."))
+		
+		err = ""
+		
+		if biaSta == QtCore.Qt.Checked and biaCou == 0:
+			err = err + "---Bias\n"
+			isBia = False
+		else:
+			isBia =True
+			
+		if darSta == QtCore.Qt.Checked and darCou == 0:
+			err = err + "---Dark\n"
+			isDar = False
+		else:
+			isDar = True
+			
+		if flaSta == QtCore.Qt.Checked and flaCou == 0:
+			err = err + "---Flat\n"
+			isFla = False
+		else:
+			isFla=True
+			
+		if isBia and isDar and isFla:
+			print("Do fotometry.")
+		else:
+			QtGui.QMessageBox.critical( self,  ("Error"), ("Oops\nSelect \n" + err + "and retry..."))
+	
+  def HeaderGetVal(self):
+	if self.ui.checkBox_5.checkState() == QtCore.Qt.Checked:
+		self.ui.lineEdit_4.setEnabled(False)
+		self.ui.comboBox_2.setEnabled(True)
+	else:
+		self.ui.lineEdit_4.setEnabled(True)
+		self.ui.comboBox_2.setEnabled(False)
+    
+  def UnlockBias(self):
+	if self.ui.checkBox.checkState() == QtCore.Qt.Checked:
+		self.ui.tabWidget_2.setTabEnabled(1,True)
+	else:
+		self.ui.tabWidget_2.setTabEnabled(1,False)
+		
+  def UnlockDark(self):
+	if self.ui.checkBox_2.checkState() == QtCore.Qt.Checked:
+		self.ui.tabWidget_2.setTabEnabled(2,True)
+	else:
+		self.ui.tabWidget_2.setTabEnabled(2,False)
+		
+  def UnlockFlat(self):
+	if self.ui.checkBox_3.checkState() == QtCore.Qt.Checked:
+		self.ui.tabWidget_2.setTabEnabled(3,True)
+	else:
+		self.ui.tabWidget_2.setTabEnabled(3,False)
+    
+#Image Add functions. Take a look to add function under functions.py file.
   def ImageAdd(self):
 	functions.add(self, self.ui.listWidget, self.ui.label_26)
-	
   def BiasAdd(self):
 	functions.add(self, self.ui.listWidget_2, self.ui.label_27)
-	
   def DarkAdd(self):
 	functions.add(self, self.ui.listWidget_3, self.ui.label_28)
-	
   def FlatAdd(self):
 	functions.add(self, self.ui.listWidget_4, self.ui.label_29)
-	
   def AlignAdd(self):
 	functions.add(self, self.ui.listWidget_13, self.ui.label_32)
-	
   def AlignManAdd(self):
 	functions.add(self, self.ui.listWidget_7, self.ui.label_31)
-	
   def PhotAdd(self):
 	functions.add(self, self.ui.listWidget_5, self.ui.label_33)
-	
   def HeaderAdd(self):
 	functions.add(self, self.ui.listWidget_9, self.ui.label_34)
 	
-    
+	
+#Image Remove functions. Take a look to Rm function under functions.py file.
   def ImageRm(self):
 	functions.Rm(self, self.ui.listWidget, self.ui.label_26)
-	
   def BiasRm(self):
 	functions.Rm(self, self.ui.listWidget_2, self.ui.label_27)
-	
   def DarkRm(self):
 	functions.Rm(self, self.ui.listWidget_3, self.ui.label_28)
-	
   def FlatRm(self):
 	functions.Rm(self, self.ui.listWidget_4, self.ui.label_29)
-	
   def AlignRm(self):
 	functions.Rm(self, self.ui.listWidget_13, self.ui.label_32)
-	
   def AlignManRm(self):
 	functions.Rm(self, self.ui.listWidget_7, self.ui.label_31)
-	
   def PhotRm(self):
 	functions.Rm(self, self.ui.listWidget_5, self.ui.label_33)
-	
   def HeaderRm(self):
 	functions.Rm(self, self.ui.listWidget_9, self.ui.label_34)
 	

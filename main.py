b252084709584b7584d810f8ba0202bed03b6caa
Yucel Kilic <yucelkilic1@gmail.com>
2019-02-27 17:15:16 +0300

    fix: bugs on pre-calibration

--- main.py
+++ main.py
@@ -1375,17 +1375,14 @@ class MyForm(QtWidgets.QWidget, Ui_Form):
                     if scl == "exposure":
                         new_file_list = []
                         for i in file_list:
-                            expt1 = self.fit.header(i, "EXPTIME")
-                            expt2 = self.fit.header(i, "EXPOSURE")
-                            expt3 = self.fit.header(
+                            expt = self.fit.header(
                                     i, self.ui.lineEdit_13.text())
-                            
-                            if expt1 is not None or expt2 is not None:
-                                new_file_list.append(i)
-                            elif expt3 is not None:
-                                self.fit.update_header(i, "EXPTIME", expt3[1])
-                                self.fit.update_header(i, "EXPOSURE", expt3[1])
-                                new_file_list.append(i)
+
+                            if expt is not None:
+                                self.fit.update_header(i, "EXPTIME", expt[1])
+                                self.fit.update_header(i, "EXPOSURE", expt[1])
+
+                            new_file_list.append(i)
                                 
                         file_list = new_file_list
                         
@@ -1466,7 +1463,6 @@ class MyForm(QtWidgets.QWidget, Ui_Form):
                         file_list = new_file_list
                         dest = "{}/{}".format(path, name)
                         
-                        
                     the_zero = None
                     if not g.is_list_empty(self, self.ui.listWidget_7):
                         answ = g.question(self, "Do you want Bias correction?")
@@ -1483,7 +1479,6 @@ class MyForm(QtWidgets.QWidget, Ui_Form):
                                 the_zero = "/tmp/myraf_bias.fits"
                             except Exception as e:
                                 self.etc.log(e)
-                            
                         
                     the_dark = None
                     if not g.is_list_empty(self, self.ui.listWidget_8):
